8d03befce351602bc67e76ff4a15381032374b31
alexlozdev <61686168+alexlozdev@users.noreply.github.com>
2022-11-01 11:35:17 -0500

    first version

---
+++ SCPDemo/pch.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <windows.h>
+#include <unknwn.h>
+#include <restrictederrorinfo.h>
+#include <hstring.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.UI.Xaml.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Windows.UI.Xaml.Data.h>
+#include <winrt/Windows.UI.Xaml.Interop.h>
+#include <winrt/Windows.UI.Xaml.Markup.h>
+#include <winrt/Windows.UI.Xaml.Navigation.h>
+
+// DirectX
+#include <d3d11_4.h>
+#include <d2d1_1.h>
+#include <windows.ui.xaml.media.dxinterop.h>
